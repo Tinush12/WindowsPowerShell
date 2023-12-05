d19be668e24363c4e526a23c218fda1790373412
Lukas Kretschmer <lukas.kretschmer@igd-r.fraunhofer.de>
2023-12-05 13:54:05 +0100

    git aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,15 +5,18 @@ Set-Alias -Name vim -Value nvim
 Set-Alias -Name l -Value list_dir
 Set-Alias -Name gs -Value git_status 
 Set-Alias -Name ga -Value git_add 
-Set-Alias -Name gitc -Value git_commit 
-Set-Alias -Name gitp -Value git_push
+Set-Alias -Name gic -Value git_commit 
+Set-Alias -Name gip -Value git_push
 
 Function git_add {
-	git add	.
+	git add .
 }
 
 Function git_commit {
-	git commit	
+    param(
+        [string]$message
+    )
+	git commit -m $message	
 }
 
 Function git_push {
@@ -21,5 +24,5 @@ Function git_push {
 }
 
 Function git_status {
-	git status 
+	git status
 }
