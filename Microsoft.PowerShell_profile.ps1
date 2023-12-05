784eb64e8b71f4923d850a9593e8da8b887deeba
Lukas Kretschmer <lukas.kretschmer@igd-r.fraunhofer.de>
2023-12-05 14:03:11 +0100

    added directory movement

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,12 +2,32 @@ Invoke-Expression (&starship init powershell)
 
 # Set-Alias -Name alias_name -Value command_name
 Set-Alias -Name vim -Value nvim
+Set-Alias -Name v -Value nvim
 Set-Alias -Name l -Value list_dir
+
+Set-Alias -Name .. -Value cd1
+Set-Alias -Name ... -Value cd2
+Set-Alias -Name .... -Value cd3
+
 Set-Alias -Name gs -Value git_status 
 Set-Alias -Name ga -Value git_add 
 Set-Alias -Name gic -Value git_commit 
 Set-Alias -Name gip -Value git_push
 
+# ----- Directory Aliases -----
+Function cd1 {
+	cd ..
+}
+
+Function cd2 {
+	cd ../..
+}
+
+Function cd3 {
+	cd ../../..
+}
+
+#----- Git Aliases -----
 Function git_add {
 	git add .
 }
